c6db7a082f9acf38e6a8f2e3865e1f1804e92222
Valdis Saules <valdis.s.coding@gmail.com>
2019-12-23 10:54:55 +0200

    added counter, button handler

--- tkinter/hellotk.py
+++ tkinter/hellotk.py
@@ -1,8 +1,21 @@
-import tkinter
+import tkinter as tk
+
+counter = 0
+
+
+def onClick():
+    print("You clicked the button")
+    global counter
+    counter += 1
+    label["text"] = f"Button was clicked {counter} times"
+
 
 if __name__ == "__main__":
-    window = tkinter.Tk()
+    window = tk.Tk()
+    window.geometry('640x480')
     window.title = "My first tkinter app"
-    but_widget = tkinter.Button(window, text="Hello RCS")
+    but_widget = tk.Button(window, text="Hello RCS", command=onClick)
     but_widget.pack()
-    tkinter.mainloop()
+    label = tk.Label(window, text='Some text:')
+    label.pack()
+    tk.mainloop()
